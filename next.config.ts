34623ad51e9ff9654d32824d7f317bda0460a6e9
sungwookoo <sungwookoo@jinhak.com>
2024-12-02 10:40:32 +0900

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: "/sungwookoo.github.io",
-  assetPrefix: "https://sungwookoo.github.io/sungwookoo.github.io", 
+  basePath: process.env.NODE_ENV === "production" ? "/sungwookoo.github.io" : "",
+  assetPrefix: process.env.NODE_ENV === "production" 
+    ? "https://sungwookoo.github.io/sungwookoo.github.io"
+    : "",
   images: {
     unoptimized: true,
     remotePatterns: [
